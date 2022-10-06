60f812aedf77a0d0fe011340cf1d761d7e01544f
Lukas Jacomin <lukasjacomin@gmail.com>
2022-10-06 02:11:07 -0300

    main component header

--- src/lib/xmonkey-component.tsx
+++ src/lib/xmonkey-component.tsx
@@ -2,9 +2,9 @@ import { VNode, VProps } from "million";
 
 export function XMonkeyComponent(props: VProps): VNode {
   return (
-    <div className="__xmwr_c d--f fd--c ai--c jc--sb">
-      <div className="xmwr-h">
-        <div className="xmwr-title">{props.title}</div>
+    <div className="xmwr_c d--f fd--c ai--c jc--sb">
+      <div className="xmwr-h w-100 m0 d--f jc--c bg-primary noselect">
+        <div className="xmwr-title m0">{props.title}</div>
       </div>
       <div className="xmwr-b">{props.children}</div>
     </div>
