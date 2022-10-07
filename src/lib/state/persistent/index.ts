0ea48af2aafab20d84c3e9a4850b88cf1da002c1
Lukas Jacomin <lukasjacomin@gmail.com>
2022-10-07 02:12:24 -0300

    no persistence implementation

--- src/lib/state/persistent/index.ts
+++ src/lib/state/persistent/index.ts
@@ -1,4 +1,5 @@
 import { AbstractPersistentState } from "./abstract-persistent-state";
 import { SessionStorageState } from "./implementations/session-storage-state";
+import { NoPersistenceState } from "./implementations/no-persistence-state";
 
-export { AbstractPersistentState, SessionStorageState };
+export { AbstractPersistentState, SessionStorageState, NoPersistenceState };
