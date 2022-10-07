78571d378b124bd057c094648dbfddc9aed09e3a
Lukas Jacomin <lukasjacomin@gmail.com>
2022-10-07 01:44:57 -0300

    script config links

--- script-config.js
+++ script-config.js
@@ -1,7 +1,13 @@
+/*
+Documentation links:
+- https://violentmonkey.github.io/api/metadata-block/
+- https://www.tampermonkey.net/documentation.php
+- https://wiki.greasespot.net/Metadata_Block
+*/
 const headers = {
   "@name": "New script",
   "@namespace": "scripts",
-  "@match": "https://*/*",
+  "@match": "https://*google.com/*",
   "@version": "1.0",
   "@author": "-",
   "@description": "Description",
