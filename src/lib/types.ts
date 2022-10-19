cb5d7d360e5ad8dd67a1b0ab4641ada623ba84c6
Lukas Jacomin <lukasjacomin@gmail.com>
2022-10-19 20:55:35 -0300

    function type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -4,3 +4,4 @@ export type Instantiable<T> = { new (...args: any[]): T };
 export type AnyType = any;
 export type AnyNullableType = AnyType | null | undefined;
 export type ClassType<T> = new () => T;
+export type FunctionType<T> = () => T;
