ab2a22100c32f6a558caa3d2bd22b0a812f6dd3c
Lukas Jacomin <lukasjacomin@gmail.com>
2024-03-10 17:22:26 -0300

    chore: eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: "tsconfig.lint.json",
+    project: "tsconfig.eslint.json",
     tsconfigRootDir: __dirname,
     sourceType: "module",
   },
