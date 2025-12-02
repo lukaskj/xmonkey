0f5272a1685fc7cc844781cc11b1e74d2b144811
Lukas K Jacomin <lukasjacomin@gmail.com>
2025-12-02 01:51:37 -0300

    fix: scss deprecated functions

--- src/build.ts
+++ src/build.ts
@@ -1,10 +1,11 @@
+#!/usr/bin/env node
 import { buildXmonkeyScript } from "./esbuild/build.js";
 
 const entryFile = process.argv[2];
 
 if (!entryFile) {
   console.error(`No entryfile specified. Try adding the script to build in the argument.`);
-  console.error(`Example: npm run build src/example-script.ts`);
+  console.error(`Example: npx xmonkey src/example-script.ts`);
   process.exit(1);
 }
 
